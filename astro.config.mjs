4fd635910fe733e8f310fd16a4c454d800f5ed17
willnfate <willnfate@gmail.com>
2026-04-24 23:08:54 +0900

    Astro - Git 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  	// 1. 사용자님의 실제 GitHub Pages 주소를 적어주세요.
+  	site: 'https://zzimkong.github.io',
+  
+  	// 2. 저장소 이름을 'base'로 설정해야 경로를 제대로 찾습니다.
+  	base: '/zzimkong_site',
+
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Zzimkong',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
